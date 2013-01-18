beef2b0ef62d9d4ad0c3e079d79b3f547c617839
Dunmail Hodkinson <dunmail@stanegate.net>
2013-01-18 07:31:27 +0000

    Refactor requestHandlers into individual modules

--- mhd.js
+++ mhd.js
@@ -1,4 +1,4 @@
 var server = require("./lib/server.js");
 var xds = require("./test/stub/xdsdocumentconsumer.js");
 
-server.start("IHE MHD service", 1337, xds);
+server.start("Mobile access to Health Documents (MHD) service", 1337, xds);
