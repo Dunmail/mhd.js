c63adadff8fb128bc0fbb8539a1a8ec2dacea5df
Dunmail Hodkinson <dunmail@stanegate.net>
2013-01-18 06:25:03 +0000

    Move xdsdocumentconsumer to stub

--- mhd.js
+++ mhd.js
@@ -1,3 +1,4 @@
 var server = require("./lib/server.js");
+var xds = require("./test/stub/xdsdocumentconsumer.js");
 
-server.start("IHE MHD service", 1337);
+server.start("IHE MHD service", 1337, xds);
