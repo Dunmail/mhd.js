1e5396e40bf0e45f0c03674165ad0b2a469d4ff2
Dunmail Hodkinson <dunmail@stanegate.net>
2013-01-20 16:53:16 +0000

    Service uses https. Self-signed cert/key provided for testing.

--- mhd.js
+++ mhd.js
@@ -1,11 +1,16 @@
 var server = require("./lib/server.js");
+var fs = require("fs");
 var xds = require("./test/stub/xdsdocumentconsumer.js");
 
-var options = {
+var config = {
 	name: "Mobile access to Health Documents (MHD) service",
 	port: 1337,
+	options: {
+	  key: fs.readFileSync("key.pem"),
+	  cert: fs.readFileSync("cert.pem")
+	},
 	xds: xds,
 	patientIdPattern: "^[0-9]{10}$"
   }
 
-server.start(options);
+server.start(config);
--- test/config/constants.js
+++ test/config/constants.js
@@ -1,4 +1,4 @@
-exports.root = "http://localhost:1337/";
+exports.root = "https://localhost:1337/";
 exports.wellformedDocumentUuid = "14a9fdec-0af4-45bb-adf2-d752b49bcc7d";
 exports.malformedDocumentUuid = "14a9fdec0af4-45bb-adf2";
 exports.unknownDocumentUuid = "10000000-0000-0000-0000-000000000000";
