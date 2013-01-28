6cc2c82dfaf69d6bf3c04dbf0d4615cf74eba086
Dunmail Hodkinson <dunmail@stanegate.net>
2013-01-28 16:58:38 +0000

    lib/xdsAdapter respects urn:uuid: convention for entry uuid

--- test/xdsAdapter.js
+++ test/xdsAdapter.js
@@ -1,36 +1,34 @@
 /*
 Requires openxds services prepopulated with documents
 */
-
 var vows = require("vows");
+var check = require("validator").check;
 var assert = require("assert");
 var libxmljs = require("libxmljs");
 var constants = require("./config/constants.js");
+
 var xds = require("../lib/xdsAdapter.js");
-
-var registryOptions = {
-  hostname: "192.168.10.65",
-  port: 2010,
-  path: "/openxds/services/DocumentRegistry/"
-};
-
-var repositoryOptions = {
-  hostname: "192.168.10.65",
-  port: 2010,
-  path: "/openxds/services/DocumentRepository/"
-};
+xds["registry"] = {
+		hostname: "192.168.10.65",
+		port: 2010,
+		path: "/openxds/services/DocumentRegistry/"}
+		
+xds["repository"] = {
+		hostname: "192.168.10.65",
+		port: 2010,
+		path: "/openxds/services/DocumentRepository/"
+		}
 
 vows.describe("xdsAdapter functional tests").addBatch({
   "when retrieving document dossier":{
   	  topic: function() {
-  	    xds.getDocumentDossier(registryOptions, constants.wellformedDocumentUuid, constants.wellformedPatientId, this.callback);
+  	    xds.getDocumentDossier(xds.registry, constants.wellformedDocumentUuid, constants.wellformedPatientId, this.callback);
   	  },
   	  "there is no error": function(err, dossier){
-  	     assert.equal(null, err);
+  	     check(err).isNull();
   	  },
   	  "dossier JSON is found": function(err, dossier){
-  	     assert.notEqual(null, dossier);
-  	     //console.log(dossier);
+  	     check(dossier).notNull();
   	  }
     }
 }).addBatch({
@@ -38,40 +36,39 @@ vows.describe("xdsAdapter functional tests").addBatch({
   	  topic: function() {
   	  	  var query = {
   	  	  };
-  	  	  xds.findDocumentDossiers(registryOptions, "http://dummy:888/", constants.wellformedPatientId, query, this.callback);
+  	  	  xds.findDocumentDossiers(xds.registry, "http://dummy:888/", constants.wellformedPatientId, query, this.callback);
   	  },
-  	  "there is no error": function(err, dossier){
-  	     assert.equal(null, err);
+  	  "there is no error": function(err, results){
+  	     check(err).isNull();
   	  },
-  	  "dossier JSON is found": function(err, dossier){
-  	     assert.notEqual(null, dossier);
+  	  "there are results": function(err, results){
+  	     check(results).notNull();
   	  }
     }
 }).addBatch({
   "when searching for document dossiers for patient with no documents":{
   	  topic: function() {
   	  	  var query = {};
-  	  	  xds.findDocumentDossiers(registryOptions, "http://dummy:888/", constants.noDocumentsPatientId, query, this.callback);
+  	  	  xds.findDocumentDossiers(xds.registry, "http://dummy:888/", constants.noDocumentsPatientId, query, this.callback);
   	  },
-  	  "the error is 'No Document Entries found' ": function(err, dossier){
-  	     assert.equal("No Document Entries found", err);
+  	  "the error is 'No Document Entries found' ": function(err, results){
+  	     check(err).is("No Document Entries found");
   	  },
-  	  "there is no dossier": function(err, dossier){
-  	     assert.equal(null, dossier);
+  	  "there is no dossier": function(err, results){
+  	     check(results).isNull();
   	  }
     }
 }).addBatch({
   "when getting document":{
   	  topic: function() {
   	  	  var query = {};
-  	  	  xds.getDocument(repositoryOptions, constants.wellformedDocumentUuid, constants.wellformedPatientId, this.callback);
+  	  	  xds.getDocument(xds.repository, constants.wellformedDocumentUuid, constants.wellformedPatientId, this.callback);
   	  },
-  	  "there is no error": function(err, dossier){
-  	     assert.equal(err, null);
+  	  "there is no error": function(err, document){
+  	     check(err).isNull;
   	  },
   	  "there is a document": function(err, document){
-  	     assert.notEqual(document, null);
-  	     
+  	     check(document.toString()).notEmpty();
   	  }
     }
 }).run();
