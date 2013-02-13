9b9dc31b28d9b22be1fdfd4c2f6e6f1798db9aa5
Dunmail Hodkinson <dunmail@stanegate.net>
2013-02-13 14:35:46 +0000

    stub/xdsAdapter can return pdf document. Use specific uuid defined in test/config/constants

--- test/stub/xdsAdapter.js
+++ test/stub/xdsAdapter.js
@@ -1,15 +1,16 @@
 var constants = require("../config/constants.js");
+var fs = require("fs");
 
-function Adapter(registry, repository){
+function Adapter(registry, repository) {
 
 }
 
 Adapter.prototype = {
-    constructor: Adapter
+    constructor:Adapter
 }
 
 //Get Document Dossier [ITI-66]
-Adapter.prototype.getDocumentDossier = function(entryUuid, patientId, callback) {
+Adapter.prototype.getDocumentDossier = function (entryUuid, patientId, callback) {
     if (entryUuid == constants.unknownDocumentUuid) {
         callback("Unknown Document UUID", null);
         return;
@@ -66,7 +67,7 @@ Adapter.prototype.getDocumentDossier = function(entryUuid, patientId, callback) 
 }
 
 //Find Document Dossiers [ITI-67]
-Adapter.prototype.findDocumentDossiers = function(params, callback) {
+Adapter.prototype.findDocumentDossiers = function (params, callback) {
     if (params.query.PatientID == constants.unknownPatientId) {
         callback("Unknown PatientID", null);
         return;
@@ -112,7 +113,8 @@ Adapter.prototype.findDocumentDossiers = function(params, callback) {
         callback(null, JSON.stringify(result));
     }
 }
-function atomise(result){ var tmp = [];
+function atomise(result) {
+    var tmp = [];
     tmp.push("<?xml version='1.0' encoding='utf-8'?>");
     tmp.push("<feed xmlns='http://www.w3.org/2005/Atom'>");
     tmp.push("<title>MHD findDocumentDossiers response</title>");
@@ -140,7 +142,7 @@ function atomise(result){ var tmp = [];
     return tmp.join("");
 }
 //Get Document [ITI-68]
-Adapter.prototype.getDocument = function(entryUuid, patientId, callback) {
+Adapter.prototype.getDocument = function (entryUuid, patientId, callback) {
     if (entryUuid == constants.unknownDocumentUuid) {
         callback("Unknown Document UUID", null);
         return;
@@ -156,88 +158,104 @@ Adapter.prototype.getDocument = function(entryUuid, patientId, callback) {
         return;
     }
 
-    var tmp = [];
-    tmp.push("<?xml version='1.0' encoding='UTF-8'?>");
-    tmp.push("<ClinicalDocument xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns='urn:hl7-org:v3'>");
-    tmp.push("<typeId extension='POCD_HD000040' root='2.16.840.1.113883.1.3'/>");
-    tmp.push("<templateId root='2.16.840.1.113883.2.1.3.9.10.0.0'/>");
-    tmp.push("<id extension='742318311954' root='2.16.840.1.113883.2.1.3.9'/>");
-    tmp.push("<code code='57027-5' codeSystem='2.16.840.1.113883.6.1' codeSystemName='LOINC' displayName='Measure Observations'/>");
-    tmp.push("<title>Measure Observations</title>");
-    tmp.push("<effectiveTime value='20080506143021+0100'/>");
-    tmp.push("<confidentialityCode code='N' codeSystem='2.16.840.1.113883.5.25' codeSystemName='HL7' displayName='Normal'/>");
-    tmp.push("<languageCode code='en-UK'/>");
-    tmp.push("<recordTarget>");
-    tmp.push("<patientRole>");
-    tmp.push("<id extension='223568611' root='2.16.840.1.113883.2.1.3.9.1.0.0'/>");
-    tmp.push("<addr><streetName>60 Hounslow Rd</streetName><city>SMYTHE'S GREEN</city><postalCode>CO5 1VO</postalCode></addr>");
-    tmp.push("<patient>");
-    tmp.push("<name><family>OWEN</family><given>OLIVER</given></name>");
-    tmp.push("<administrativeGenderCode code='M' codeSystem='2.16.840.1.113883.5.1'/>");
-    tmp.push("<birthTime value='19331210'/>");
-    tmp.push("</patient>");
-    tmp.push("</patientRole>");
-    tmp.push("</recordTarget>");
-    tmp.push("<author>");
-    tmp.push("<time value='20080506143021+0100'/>");
-    tmp.push("<assignedAuthor>");
-    tmp.push("<id extension='593978059683' root='2.16.840.1.113883.2.1.3.9.1.0.0'/>");
-    tmp.push("<assignedPerson>");
-    tmp.push("<name><family>HOOK</family><given>JAMES</given></name>");
-    tmp.push("</assignedPerson>");
-    tmp.push("<representedOrganization>");
-    tmp.push("<id root='2.16.840.1.113883.2.1.3.9.19.5'/>");
-    tmp.push("<name>Good Health Clinic</name>");
-    tmp.push("</representedOrganization>");
-    tmp.push("</assignedAuthor>");
-    tmp.push("</author>");
-    tmp.push("<custodian>");
-    tmp.push("<assignedCustodian>");
-    tmp.push("<representedCustodianOrganization>");
-    tmp.push("<id root='2.16.840.1.113883.2.1.3.9.1.1.1.2.0'/>");
-    tmp.push("<name>Sintero XDS</name>");
-    tmp.push("</representedCustodianOrganization>");
-    tmp.push("</assignedCustodian>");
-    tmp.push("</custodian>");
-    tmp.push("<documentationOf>");
-    tmp.push("<serviceEvent classCode='MPROT'>");
-    tmp.push("<effectiveTime>");
-    tmp.push("<low value='20080506141821'/>");
-    tmp.push("<high value='20080506144221+0100'/>");
-    tmp.push("</effectiveTime>");
-    tmp.push("</serviceEvent>");
-    tmp.push("</documentationOf>");
-    tmp.push("<component>");
-    tmp.push("<structuredBody>");
-    tmp.push("<component>");
-    tmp.push("<section>");
-    tmp.push("<templateId root='2.16.840.1.113883.2.1.3.9.10.1.1'/>");
-    tmp.push("<code code='2276-4' codeSystem='2.16.840.1.113883.6.1' codeSystemName='LOINC' displayName='Ferritin'/>");
-    tmp.push("<text>06-May-2008 13:30:21 GMT+01:00 Ferritin: 622.0 ug/L</text>");
-    tmp.push("<entry>");
-    tmp.push("<observation classCode='OBS' moodCode='EVN'>");
-    tmp.push("<code code='2276-4' codeSystem='2.16.840.1.113883.6.1' codeSystemName='LOINC' displayName='Ferritin'/>");
-    tmp.push("<effectiveTime value='20080506141821'/>");
-    tmp.push("<value xsi:type='PQ' unit='ug/L' value='622.0'/>");
-    tmp.push("</observation>");
-    tmp.push("</entry>");
-    tmp.push("</section>");
-    tmp.push("</component>");
-    tmp.push("</structuredBody>");
-    tmp.push("</component>");
-    tmp.push("</ClinicalDocument>");
-    var data = tmp.join("");
+    if (entryUuid == constants.pdfDocumentUuid) {
+        fs.readFile('./doc/IHE_ITI_Suppl_MHD.pdf', function (err, data) {
+            if (err) throw err;
 
-    var document = {
-        headers:{
-            "content-type":"text/xml",
-            "content-transfer-encoding":"binary",
-            "content-id":"<1.urn:uuid:BA2B74DD7D946FD24E1359170843006@apache.org>"
-        },
-        data:data
+            var document = {
+                headers:{
+                    "content-type":"application/pdf",
+                    "content-transfer-encoding":"binary",
+                    "content-id":"<1.urn:uuid:BA2B74DD7D946FD24E1359170843006@apache.org>"
+                },
+                data:data};
+            callback(null, document); //async
+        });
     }
+    else {
+        var tmp = [];
+        tmp.push("<?xml version='1.0' encoding='UTF-8'?>");
+        tmp.push("<ClinicalDocument xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns='urn:hl7-org:v3'>");
+        tmp.push("<typeId extension='POCD_HD000040' root='2.16.840.1.113883.1.3'/>");
+        tmp.push("<templateId root='2.16.840.1.113883.2.1.3.9.10.0.0'/>");
+        tmp.push("<id extension='742318311954' root='2.16.840.1.113883.2.1.3.9'/>");
+        tmp.push("<code code='57027-5' codeSystem='2.16.840.1.113883.6.1' codeSystemName='LOINC' displayName='Measure Observations'/>");
+        tmp.push("<title>Measure Observations</title>");
+        tmp.push("<effectiveTime value='20080506143021+0100'/>");
+        tmp.push("<confidentialityCode code='N' codeSystem='2.16.840.1.113883.5.25' codeSystemName='HL7' displayName='Normal'/>");
+        tmp.push("<languageCode code='en-UK'/>");
+        tmp.push("<recordTarget>");
+        tmp.push("<patientRole>");
+        tmp.push("<id extension='223568611' root='2.16.840.1.113883.2.1.3.9.1.0.0'/>");
+        tmp.push("<addr><streetName>60 Hounslow Rd</streetName><city>SMYTHE'S GREEN</city><postalCode>CO5 1VO</postalCode></addr>");
+        tmp.push("<patient>");
+        tmp.push("<name><family>OWEN</family><given>OLIVER</given></name>");
+        tmp.push("<administrativeGenderCode code='M' codeSystem='2.16.840.1.113883.5.1'/>");
+        tmp.push("<birthTime value='19331210'/>");
+        tmp.push("</patient>");
+        tmp.push("</patientRole>");
+        tmp.push("</recordTarget>");
+        tmp.push("<author>");
+        tmp.push("<time value='20080506143021+0100'/>");
+        tmp.push("<assignedAuthor>");
+        tmp.push("<id extension='593978059683' root='2.16.840.1.113883.2.1.3.9.1.0.0'/>");
+        tmp.push("<assignedPerson>");
+        tmp.push("<name><family>HOOK</family><given>JAMES</given></name>");
+        tmp.push("</assignedPerson>");
+        tmp.push("<representedOrganization>");
+        tmp.push("<id root='2.16.840.1.113883.2.1.3.9.19.5'/>");
+        tmp.push("<name>Good Health Clinic</name>");
+        tmp.push("</representedOrganization>");
+        tmp.push("</assignedAuthor>");
+        tmp.push("</author>");
+        tmp.push("<custodian>");
+        tmp.push("<assignedCustodian>");
+        tmp.push("<representedCustodianOrganization>");
+        tmp.push("<id root='2.16.840.1.113883.2.1.3.9.1.1.1.2.0'/>");
+        tmp.push("<name>Sintero XDS</name>");
+        tmp.push("</representedCustodianOrganization>");
+        tmp.push("</assignedCustodian>");
+        tmp.push("</custodian>");
+        tmp.push("<documentationOf>");
+        tmp.push("<serviceEvent classCode='MPROT'>");
+        tmp.push("<effectiveTime>");
+        tmp.push("<low value='20080506141821'/>");
+        tmp.push("<high value='20080506144221+0100'/>");
+        tmp.push("</effectiveTime>");
+        tmp.push("</serviceEvent>");
+        tmp.push("</documentationOf>");
+        tmp.push("<component>");
+        tmp.push("<structuredBody>");
+        tmp.push("<component>");
+        tmp.push("<section>");
+        tmp.push("<templateId root='2.16.840.1.113883.2.1.3.9.10.1.1'/>");
+        tmp.push("<code code='2276-4' codeSystem='2.16.840.1.113883.6.1' codeSystemName='LOINC' displayName='Ferritin'/>");
+        tmp.push("<text>06-May-2008 13:30:21 GMT+01:00 Ferritin: 622.0 ug/L</text>");
+        tmp.push("<entry>");
+        tmp.push("<observation classCode='OBS' moodCode='EVN'>");
+        tmp.push("<code code='2276-4' codeSystem='2.16.840.1.113883.6.1' codeSystemName='LOINC' displayName='Ferritin'/>");
+        tmp.push("<effectiveTime value='20080506141821'/>");
+        tmp.push("<value xsi:type='PQ' unit='ug/L' value='622.0'/>");
+        tmp.push("</observation>");
+        tmp.push("</entry>");
+        tmp.push("</section>");
+        tmp.push("</component>");
+        tmp.push("</structuredBody>");
+        tmp.push("</component>");
+        tmp.push("</ClinicalDocument>");
+        var data = tmp.join("");
 
-    callback(null, document);
+        var document = {
+            headers:{
+                "content-type":"text/xml",
+                "content-transfer-encoding":"binary",
+                "content-id":"<1.urn:uuid:BA2B74DD7D946FD24E1359170843006@apache.org>"
+            },
+            data:data
+        }
+
+        callback(null, document); //sync
+    }
 }
 
-exports.Adapter=Adapter;
+exports.Adapter = Adapter;
